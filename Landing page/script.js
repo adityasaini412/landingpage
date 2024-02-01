82bb160e8144b4caa2149099997ea3b5a89b3897
adityasaini412 <154610798+adityasaini412@users.noreply.github.com>
2024-02-01 15:25:44 +0530

    Add files via upload

---
+++ Landing page/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.span', {
+    strings: ['LEARN', 
+               'GROW',
+               'CONNECT',
+               'COMPETE' ],
+    typeSpeed:60,
+    backSpeed:60,
+    loop:true,
+  });
